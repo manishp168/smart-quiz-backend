22a46d13c01c878db17af80d8cf947161681a43d
Manish Choudhary <manishpotliya1@gmail.com>
2025-04-12 12:37:33 +0530

    add vercel domain to allow origin

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from "express";
 import cors from 'cors';
 const app = express();
 
-const allowedOrigins = ["http://localhost:5173", "http://earningzone.shop", "https://earningzone.shop"];
+const allowedOrigins = ["http://localhost:5173", "http://earningzone.shop", "https://earningzone.shop", "https://smartquiz-chi.vercel.app", "http://smartquiz-chi.vercel.app/"];
 app.use(cors({
     origin: function (origin, callback) {
         if (!origin || allowedOrigins.includes(origin)) {
