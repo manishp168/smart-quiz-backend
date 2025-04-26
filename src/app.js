e45257c2a32d829134492f0b64bdf8aaaf35d47f
Manish Choudhary <manishpotliya1@gmail.com>
2025-04-26 09:42:24 +0530

    allow one more origin

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from "express";
 import cors from 'cors';
 const app = express();
 
-const allowedOrigins = ["http://localhost:5173", "http://earningzone.shop", "https://earningzone.shop", "https://smartquiz-chi.vercel.app", "http://smartquiz-chi.vercel.app/"];
+const allowedOrigins = ["http://localhost:5173", "http://smart-quizz.vercel.app", "https://smart-quizz.vercel.app", "https://smartquiz-chi.vercel.app", "http://smartquiz-chi.vercel.app"];
 app.use(cors({
     origin: function (origin, callback) {
         if (!origin || allowedOrigins.includes(origin)) {
